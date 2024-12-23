4f13dfd5cc62c49b497f3fb50000e512d7605d68
rohit02k5 <rohitiitbh@gmail.com>
2024-12-23 11:52:07 +0530

    3rd commit

--- src/Pages/CreatePost.jsx
+++ src/Pages/CreatePost.jsx
@@ -17,7 +17,7 @@ const CreatePost = () => {
         try {
           setGenerateImg(true);
     
-          const response = await fetch('http://localhost:8080/api/v1/huggingface', {
+          const response = await fetch('https://neurapix-backend.onrender.com/api/v1/huggingface', {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json',
@@ -49,7 +49,7 @@ const CreatePost = () => {
       if (form.prompt && form.photo) {
         setLoading(true);
         try {
-          const response = await fetch('http://localhost:8080/api/v1/post', {
+          const response = await fetch('https://neurapix-backend.onrender.com/api/v1/post', {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json',
